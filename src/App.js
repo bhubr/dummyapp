62fcd1cd4543142be60aba38047a6d75401c7d52
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-05-15 18:04:35 +0200

    Get stuff from remote API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [sessions, setSessions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://wcs-support.byethost22.com/session/index')
+      .then(res => res.json())
+      .then(data => setSessions(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        sessions.map((sess) => (
+          <p key={sess.id}>{sess.title}</p>
+        ))
+      }
     </div>
   );
 }
