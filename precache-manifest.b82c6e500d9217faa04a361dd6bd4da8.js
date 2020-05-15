6a1662283ce9b3e818d168b0f4c36f6d88d2dfb1
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-05-15 18:08:02 +0200

    Updates

---
+++ precache-manifest.b82c6e500d9217faa04a361dd6bd4da8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7981a0285989aeb87856a451735f667b",
+    "url": "/reactphpcors/index.html"
+  },
+  {
+    "revision": "f7f4ac9f5e5680bfe4f5",
+    "url": "/reactphpcors/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f0056ff9e366f930451",
+    "url": "/reactphpcors/static/js/2.c36828ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactphpcors/static/js/2.c36828ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f4ac9f5e5680bfe4f5",
+    "url": "/reactphpcors/static/js/main.0b72c9f7.chunk.js"
+  },
+  {
+    "revision": "190282d9904e405f199d",
+    "url": "/reactphpcors/static/js/runtime-main.b22338f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactphpcors/static/media/logo.5d5d9eef.svg"
+  }
+]);
